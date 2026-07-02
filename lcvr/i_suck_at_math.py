0c4479c65cc110974760c2a4b77e0e471f6f586b
Arin <arin.jander@gmail.com>
2026-07-02 15:21:11 -0700

    do some maths

---
+++ lcvr/i_suck_at_math.py
@@ -0,0 +1,8 @@
+"""
+standard equation
+given voltage V1 -> retardance D1 @ W1 (wavelength)
+Dcomp = standard 'compensator' offset
+R = bifringence dispersion ratio; RLC = (deltaN_LC(W2)/deltaN_LC(W1))
+voltage V1 - > retardance D2 = (D1 + Dcomp) * (W1/W2) * RLC - Dcomp * (W1/W2) * Rcomp  @ W2
+"""
+## TODO: ^^^^^ check it plz ~Arin
